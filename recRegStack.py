59518a5dcc7633c998516abbad95b9a814cee67f
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2016-11-30 10:42:51 +0100

    recRegStack.py based on SimpleElastix.py

---
+++ recRegStack.py
@@ -0,0 +1,19 @@
+## https://github.com/kaspermarstal/SimpleElastix/blob/master/Examples/Python/SimpleElastix.py
+
+import SimpleITK as sitk
+import sys
+
+# Instantiate SimpleElastix
+selx = sitk.SimpleElastix()
+
+# Read Input
+selx.SetFixedImage(sitk.ReadImage(str(sys.argv[1])))
+selx.SetMovingImage(sitk.ReadImage(str(sys.argv[2])))
+selx.SetParameterMap(sitk.ReadParameterFile(str(sys.argv[3])))
+
+# Perform registration
+selx.LogToConsoleOn()
+selx.Execute()
+
+# Write result image
+sitk.WriteImage(selx.GetResultImage(), str(sys.argv[4]))
