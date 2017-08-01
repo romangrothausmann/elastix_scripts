1de0089566f215ab85e153c869fa35fc27b4c1f1
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2017-08-01 08:56:30 +0200

    intro. main()

--- recRegStack.py
+++ recRegStack.py
@@ -42,79 +42,84 @@ def stdout_redirected(to=os.devnull, stdout=None):
 ### END https://stackoverflow.com/questions/4675728/redirect-stdout-to-a-file-in-python#22434262
 
 
-FNs= sorted( glob.glob(sys.argv[1]) ) # http://stackoverflow.com/questions/6773584/how-is-pythons-glob-glob-ordered # http://stackoverflow.com/questions/3207219/how-to-list-all-files-of-a-directory-in-python#3215392
-FNo= sys.argv[2] # FNo= os.path.abspath(FNs[0]) + "/reg/"
-if not os.path.exists(FNo): # http://stackoverflow.com/questions/273192/how-to-check-if-a-directory-exists-and-create-it-if-necessary
-    os.makedirs(FNo)
+def main():
 
-start= int(sys.argv[4])
+    FNs= sorted( glob.glob(sys.argv[1]) ) # http://stackoverflow.com/questions/6773584/how-is-pythons-glob-glob-ordered # http://stackoverflow.com/questions/3207219/how-to-list-all-files-of-a-directory-in-python#3215392
+    FNo= sys.argv[2] # FNo= os.path.abspath(FNs[0]) + "/reg/"
+    if not os.path.exists(FNo): # http://stackoverflow.com/questions/273192/how-to-check-if-a-directory-exists-and-create-it-if-necessary
+        os.makedirs(FNo)
 
-## copy first file as is or transform with identity
-for idx, FN in enumerate(FNs):
+    start= int(sys.argv[4])
 
-    ## skip upto start:
-    if(idx < start):
-        continue
-    
-    FN0= FNs[(idx - 1) % len(FNs)] # http://stackoverflow.com/questions/2167868/getting-next-element-while-cycling-through-a-list#2167962
-    FN1= FN
-    FNof= sys.argv[2] + "/" + os.path.splitext(FN1)[0] + ".tif" # TIF for float # http://stackoverflow.com/questions/678236/how-to-get-the-filename-without-the-extension-from-a-path-in-python
-    FNit= os.path.splitext(FN1)[0] + ".txt"
-    FNt= sys.argv[2] + "/" + os.path.splitext(FN1)[0] + ".txt"
-    DNl= sys.argv[2] + "/" + os.path.splitext(FN1)[0] + ".log/"
+    ## copy first file as is or transform with identity
+    for idx, FN in enumerate(FNs):
 
-    # Instantiate SimpleElastix
-    selx = sitk.ElastixImageFilter() # https://github.com/SuperElastix/SimpleElastix/issues/99#issuecomment-308132783
-    selx.LogToFileOff()
-    selx.LogToConsoleOn()
-    
-    pM= selx.ReadParameterFile(str(sys.argv[3])) # https://github.com/kaspermarstal/SimpleElastix/blob/master/Code/Elastix/include/sitkSimpleElastix.h#L119
+        ## skip upto start:
+        if(idx < start):
+            continue
 
-    if not os.path.exists(DNl):
-        os.makedirs(DNl)
-    selx.SetOutputDirectory(DNl)
+        FN0= FNs[(idx - 1) % len(FNs)] # http://stackoverflow.com/questions/2167868/getting-next-element-while-cycling-through-a-list#2167962
+        FN1= FN
+        FNof= sys.argv[2] + "/" + os.path.splitext(FN1)[0] + ".tif" # TIF for float # http://stackoverflow.com/questions/678236/how-to-get-the-filename-without-the-extension-from-a-path-in-python
+        FNit= os.path.splitext(FN1)[0] + ".txt"
+        FNt= sys.argv[2] + "/" + os.path.splitext(FN1)[0] + ".txt"
+        DNl= sys.argv[2] + "/" + os.path.splitext(FN1)[0] + ".log/"
 
-    elastixLog= os.path.splitext(FN1)[0] + ".log"
-    selx.SetLogFileName(elastixLog)
-    elastixLogPath= DNl + elastixLog
+        # Instantiate SimpleElastix
+        selx = sitk.ElastixImageFilter() # https://github.com/SuperElastix/SimpleElastix/issues/99#issuecomment-308132783
+        selx.LogToFileOff()
+        selx.LogToConsoleOn()
 
-    print("\r%5.1f%% (%d/%d)" % ((idx+1) * 100.0 / len(FNs), idx+1, len(FNs))),
-    sys.stdout.flush() # essential with \r !
-    
-    if idx == 0:
-        sitk.WriteImage(sitk.Cast(sitk.ReadImage(FN1), sitk.sitkUInt8), FNof)
-        continue
-    else:
-        FN0= sys.argv[2] + "/" + os.path.splitext(FN0)[0] + ".tif"
-    
+        pM= selx.ReadParameterFile(str(sys.argv[3])) # https://github.com/kaspermarstal/SimpleElastix/blob/master/Code/Elastix/include/sitkSimpleElastix.h#L119
 
-    fI= sitk.ReadImage(FN0)
-    mI= sitk.ReadImage(FN1)
+        if not os.path.exists(DNl):
+            os.makedirs(DNl)
+        selx.SetOutputDirectory(DNl)
 
-    selx.SetFixedImage(fI) # https://github.com/kaspermarstal/SimpleElastix/blob/master/Code/IO/include/sitkImageFileReader.h#L73
-    selx.SetFixedMask(fI != 0)
-    selx.SetMovingImage(mI)
-    pM.erase('InitialTransformParametersFileName')
-    selx.SetParameterMap(pM)
-    if os.path.isfile(FNit):
-        selx.SetInitialTransformParameterFileName(FNit)
-        print selx.GetInitialTransformParameterFileName()
-    with open(elastixLogPath, 'w') as f, stdout_redirected(f):
-        selx.Execute()
-    f.close()
+        elastixLog= os.path.splitext(FN1)[0] + ".log"
+        selx.SetLogFileName(elastixLog)
+        elastixLogPath= DNl + elastixLog
 
-    finalMetricValue= 0
-    with open(elastixLogPath) as f:
-        m= re.search('Final metric value  = (?P<value>[+-.0-9]{9})', f.read()) # http://lists.bigr.nl/pipermail/elastix/2016-December/002435.html
-    f.close()
-    if m:
-        try:
-            finalMetricValue= float(m.group('value'))
-        except:
-            raise Exception('Final metric value not found in "elastix.log".')
-    print(finalMetricValue)
+        print("\r%5.1f%% (%d/%d)" % ((idx+1) * 100.0 / len(FNs), idx+1, len(FNs))),
+        sys.stdout.flush() # essential with \r !
 
-    # Write result image
-    sitk.WriteImage(sitk.Cast(selx.GetResultImage(), sitk.sitkUInt8), FNof)
-    selx.WriteParameterFile(selx.GetTransformParameterMap(0), FNt)
+        if idx == 0:
+            sitk.WriteImage(sitk.Cast(sitk.ReadImage(FN1), sitk.sitkUInt8), FNof)
+            continue
+        else:
+            FN0= sys.argv[2] + "/" + os.path.splitext(FN0)[0] + ".tif"
 
+
+        fI= sitk.ReadImage(FN0)
+        mI= sitk.ReadImage(FN1)
+
+        selx.SetFixedImage(fI) # https://github.com/kaspermarstal/SimpleElastix/blob/master/Code/IO/include/sitkImageFileReader.h#L73
+        selx.SetFixedMask(fI != 0)
+        selx.SetMovingImage(mI)
+        pM.erase('InitialTransformParametersFileName')
+        selx.SetParameterMap(pM)
+        if os.path.isfile(FNit):
+            selx.SetInitialTransformParameterFileName(FNit)
+            print selx.GetInitialTransformParameterFileName()
+        with open(elastixLogPath, 'w') as f, stdout_redirected(f):
+            selx.Execute()
+        f.close()
+
+        finalMetricValue= 0
+        with open(elastixLogPath) as f:
+            m= re.search('Final metric value  = (?P<value>[+-.0-9]{9})', f.read()) # http://lists.bigr.nl/pipermail/elastix/2016-December/002435.html
+        f.close()
+        if m:
+            try:
+                finalMetricValue= float(m.group('value'))
+            except:
+                raise Exception('Final metric value not found in "elastix.log".')
+        print(finalMetricValue)
+
+        # Write result image
+        sitk.WriteImage(sitk.Cast(selx.GetResultImage(), sitk.sitkUInt8), FNof)
+        selx.WriteParameterFile(selx.GetTransformParameterMap(0), FNt)
+
+
+if __name__ == "__main__":
+    main()
