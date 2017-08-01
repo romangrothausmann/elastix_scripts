7c8fab622337abecc29b67ad0cc62508c1284b3c
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2017-08-01 10:28:42 +0200

    Start specification now by file-name (not index)

--- recRegStack.py
+++ recRegStack.py
@@ -53,20 +53,23 @@ def main():
     parser.add_argument("-i", "--inputPattern", dest="input", metavar='GlobPattern', required=True, help="Glob-pattern for input files.")
     parser.add_argument("-o", "--output", dest="output", metavar='DestDir', required=True, help="Output dir to save the result images in.")
     parser.add_argument("-p", "--paramFile", dest="PF", metavar='ParamFile', required=True, help="Elastix Parameter File")
-    parser.add_argument("-s", "--start", dest="start", type=int, required=False, help="Skip images before Start value.")
+    parser.add_argument("-s", "--start", dest="start", required=False, help="Skip images before specified start-file.")
     parser.add_argument("-S", "--skip", dest="skip", metavar='N', nargs='+', help="Skip specified file-names.")
 
 
     args = parser.parse_args()
 
     FNs= sorted( glob.glob(args.input) ) # http://stackoverflow.com/questions/6773584/how-is-pythons-glob-glob-ordered # http://stackoverflow.com/questions/3207219/how-to-list-all-files-of-a-directory-in-python#3215392
-    FNs= [x for x in FNs if x not in args.skip] # indexing can be problematic: [FNs.pop(x) for x in args.skip]
+    FNs= [x for x in FNs if x not in args.skip] # removing by index (less suitable): [FNs.pop(x) for x in args.skip]
+    try:
+        start= FNs.index(args.start) # can be used as FNs= FNs[start:] but not ideal for referencing idx-1
+    except ValueError, e:
+        start= None
+        print "Start not found!"
     
     FNo= args.output # FNo= os.path.abspath(FNs[0]) + "/reg/"
     if not os.path.exists(FNo): # http://stackoverflow.com/questions/273192/how-to-check-if-a-directory-exists-and-create-it-if-necessary
         os.makedirs(FNo)
-
-    start= args.start
 
     stfx = sitk.TransformixImageFilter() # stfx instanciated inside loop causes segfault on second execution
     stfx.LogToFileOff()
